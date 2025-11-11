e428661aefc6f4d444ff111ace95f35e7be98ebe
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-11-11 18:55:43 +0000

    changes

---
+++ sum_of_numbers.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Created By: Brandon
+# Date: November 11th, 2025
+# This program asks the user for the number and then sums all the numbers up
+
+
+def main():
+
+    # get the age from the user
+    number = input("Enter a positive number: ")
+
+    # initialize counter and sum
+    counter = 0
+    sum = 0
+
+    # Checking if the user entered an integer correctly
+    try:
+        number = int(number)
+        print("You entered a positive number!")
+
+        # determine whether or the not the number is positive
+        if number < 0:
+            print("Please Enter a positive number")
+        else:
+            while counter <= number:
+                sum = sum + counter
+                counter = counter + 1
+
+    except ValueError:
+        print("That is not a valid integer")
+
+    print(sum)
+
+
+# outputs the function
+if __name__ == "__main__":
+    main()
